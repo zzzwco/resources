375452d3322b4609cf8b08b02c1c376e33baf455
Bruce <zzzwco@outlook.com>
2023-04-03 11:29:14 +0800

    Update Swift5.8

--- Swift-language/Swift5.8/main.swift
+++ Swift-language/Swift5.8/main.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 // MARK: - 闭包内隐式调用 self
 
@@ -53,4 +54,43 @@ extension Temperature {
     return (degreesCelsius * 9 / 5) + 32
   }
 }
+
+// MARK: - 集合类型支持向下类型转换
+
+class A {}
+class A1: A {}
+class A2: A {}
+
+func downcast(arr: [A]) {
+  switch arr {
+  case let a1 as [A1]:
+    print("arr is [A1]: \(a1)")
+  case let a2 as [A2]:
+    print("arr is [A2]: \(a2)")
+  default:
+    print("Unknown collection type")
+  }
+}
+
+// MARK: - #file & #filePath
+
+print(#file)
+print(#filePath)
+
+// MARK: - 解除结果生成器中变量的使用限制
+
+struct ContentView: View {
+  
+  var body: some View {
+    lazy var v1: Int = .random(in: 0...10)
+    var v2: Int { v1 * v1 }
+    Text("v1: \(v1)") +
+    Text("v2: \(v2)")
+  }
+}
+
+// MARK: - 支持 Swift-DocC 生成关于 extensions 的文档
+
+// $ swift package generate-documentation --include-extended-types
+
 
