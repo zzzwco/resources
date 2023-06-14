08e55f6647beb74a3208b01c2204f0e245f02e2b
Bruce <zzzwco@outlook.com>
2023-06-14 18:19:17 +0800

    Add Swift5.9

---
+++ Swift-language/Swift5.9/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Swift5.9
+//
+//  Created by Bruce on 2023/6/8.
+//
+
+import Foundation
+
+// MARK: - if 和 switch 语句作为表达式
+
+let s1 = if Bool.random() { "YES" } else { "NO" }
+
+let s2 = switch Bool.random() {
+case true: "YES"
+case false: "NO"
+}
+
+
+
